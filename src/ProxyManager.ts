a4323f5555d6295a48ea462c128b84906cd70605
FTWinston <ftwinston@gmail.com>
2021-06-05 21:36:49 +0100

    Now using different proxies for objects, arrays, maps and sets

--- src/ProxyManager.ts
+++ src/ProxyManager.ts
@@ -1,4 +1,4 @@
-import { isArray } from 'enhancejson/lib/typeChecks';
+import { isArray, isMap, isSet } from 'enhancejson/lib/typeChecks';
 import type { Operation } from './Operation';
 import { OperationType } from './OperationType';
 
@@ -18,30 +18,67 @@ export class ProxyManager {
         private readonly patchCallback: (operation: Operation) => void
     ) {}
 
-    public createProxy(underlying: BaseObject, path: string) {
-        const proxiedChildren = new Set<BaseObject>();
+    private getField(
+        path: string,
+        field: string,
+        val: any,
+        proxiedChildren: Set<BaseObject>
+    ) {
+        const fieldProxyInfo = this.proxies.get(val);
 
-        const proxy: BaseObject = new Proxy(underlying, {
+        if (fieldProxyInfo) {
+            return fieldProxyInfo.proxy;
+        }
+
+        if (this.canProxy(val)) {
+            proxiedChildren.add(val);
+
+            const childPath = path ? `${path}/${field}` : field;
+
+            return this.createProxy(val, childPath);
+        }
+
+        return val;
+    }
+
+    private setField(
+        path: string,
+        field: string,
+        val: any,
+        prevVal: any,
+        proxiedChildren: Set<BaseObject>
+    ) {
+        proxiedChildren.delete(prevVal);
+        this.removeProxy(prevVal);
+
+        this.patchCallback(this.createSetOperation(path, field, val));
+    }
+
+    private deleteField(
+        path: string,
+        field: string,
+        val: any,
+        proxiedChildren: Set<BaseObject>
+    ) {
+        proxiedChildren.delete(val);
+        this.removeProxy(val);
+
+        this.patchCallback(this.createDeleteOperation(path, field));
+    }
+
+    private createObjectHandler(
+        path: string,
+        proxiedChildren: Set<BaseObject>
+    ): ProxyHandler<BaseObject> {
+        return {
             get: (target, field) => {
-                const val = (target as any)[field];
-
-                let fieldProxy: BaseObject | undefined;
+                let val = (target as any)[field];
 
                 if (typeof field === 'string' && field !== 'prototype') {
-                    const fieldProxyInfo = this.proxies.get(val);
-
-                    if (fieldProxyInfo) {
-                        fieldProxy = fieldProxyInfo.proxy;
-                    } else if (this.canProxy(val)) {
-                        const childPath =
-                            path === '' ? field : `${path}/${field}`;
-
-                        fieldProxy = this.createProxy(val, childPath);
-                        proxiedChildren.add(val);
-                    }
+                    val = this.getField(path, field, val, proxiedChildren);
                 }
 
-                return fieldProxy === undefined ? val : fieldProxy;
+                return val;
             },
             set: (target, field, val) => {
                 const prevVal = (target as any)[field];
@@ -49,15 +86,7 @@ export class ProxyManager {
                 (target as any)[field] = val;
 
                 if (typeof field === 'string') {
-                    proxiedChildren.delete(prevVal);
-                    this.removeProxy(prevVal);
-
-                    // Don't record array length changes. TODO: separate array proxy I guess.
-                    if (!isArray(target) || field !== 'length') {
-                        this.patchCallback(
-                            this.createSetOperation(path, field, val)
-                        );
-                    }
+                    this.setField(path, field, val, prevVal, proxiedChildren);
                 }
 
                 return true;
@@ -68,15 +97,201 @@ export class ProxyManager {
                 delete (target as any)[field];
 
                 if (typeof field === 'string') {
-                    proxiedChildren.delete(val);
-                    this.removeProxy(val);
-
-                    this.patchCallback(this.createDeleteOperation(path, field));
+                    this.deleteField(path, field, val, proxiedChildren);
                 }
 
                 return true;
             },
-        });
+        };
+    }
+
+    private createArrayHandler(
+        path: string,
+        proxiedChildren: Set<BaseObject>
+    ): ProxyHandler<any[]> {
+        return {
+            get: (target, field) => {
+                let val = (target as any)[field];
+
+                if (typeof field === 'string' && field !== 'prototype') {
+                    val = this.getField(path, field, val, proxiedChildren);
+                }
+
+                return val;
+            },
+            set: (target, field, val) => {
+                const prevVal = (target as any)[field];
+
+                (target as any)[field] = val;
+
+                // Don't record array length changes.
+                if (typeof field === 'string' && field !== 'length') {
+                    this.setField(path, field, val, prevVal, proxiedChildren);
+                }
+
+                return true;
+            },
+            deleteProperty: (target, field) => {
+                const val = (target as any)[field];
+
+                delete (target as any)[field];
+
+                if (typeof field === 'string') {
+                    this.deleteField(path, field, val, proxiedChildren);
+                }
+
+                return true;
+            },
+        };
+    }
+
+    private isAllowedMapKey(key: any) {
+        switch (typeof key) {
+            case 'string':
+            case 'number':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    private createMapHandler(
+        path: string,
+        proxiedChildren: Set<BaseObject>
+    ): ProxyHandler<Map<any, any>> {
+        return {
+            get: (target, field) => {
+                let func;
+
+                if (field === 'get') {
+                    func = (key: any) => {
+                        let val = target.get(key);
+
+                        if (this.isAllowedMapKey(key)) {
+                            val = this.getField(
+                                path,
+                                field,
+                                val,
+                                proxiedChildren
+                            );
+                        }
+
+                        return val;
+                    };
+                } else if (field === 'set') {
+                    func = (key: any, val: any) => {
+                        const prevVal = target.get(key);
+
+                        target.set(key, val);
+
+                        if (this.isAllowedMapKey(key)) {
+                            this.setField(
+                                path,
+                                key,
+                                val,
+                                prevVal,
+                                proxiedChildren
+                            );
+                        }
+
+                        return this;
+                    };
+                } else if (field === 'delete') {
+                    func = (key: any) => {
+                        const val = target.get(key);
+
+                        target.delete(key);
+
+                        if (this.isAllowedMapKey(key)) {
+                            this.deleteField(path, field, val, proxiedChildren);
+                        }
+
+                        return this;
+                    };
+                } else if (field === 'clear') {
+                    func = () => {
+                        target.clear();
+                        // TODO: generate patch
+                    };
+                } else {
+                    let val = (target as any)[field];
+
+                    if (typeof val === 'function') {
+                        val = val.bind(target);
+                    }
+
+                    return val;
+                }
+
+                return func.bind(target);
+            },
+        };
+    }
+
+    private createSetHandler(
+        path: string,
+        proxiedChildren: Set<BaseObject>
+    ): ProxyHandler<Set<any>> {
+        // For patch purposes treat Sets like Maps, use the values as keys and always use "1" as the pretend value.
+        return {
+            get: (target, field) => {
+                let func;
+
+                if (field === 'add') {
+                    func = (val: any) => {
+                        target.add(val);
+
+                        if (this.isAllowedMapKey(val)) {
+                            this.setField(path, val, 1, 1, proxiedChildren);
+                        }
+
+                        return this;
+                    };
+                } else if (field === 'delete') {
+                    func = (key: any) => {
+                        target.delete(key);
+
+                        if (this.isAllowedMapKey(key)) {
+                            this.deleteField(path, field, 1, proxiedChildren);
+                        }
+
+                        return this;
+                    };
+                } else if (field === 'clear') {
+                    func = () => {
+                        target.clear();
+                        // TODO: generate patch
+                    };
+                } else {
+                    let val = (target as any)[field];
+
+                    if (typeof val === 'function') {
+                        val = val.bind(target);
+                    }
+
+                    return val;
+                }
+
+                return func.bind(target);
+            },
+        };
+    }
+
+    public createProxy(underlying: BaseObject, path: string) {
+        const proxiedChildren = new Set<BaseObject>();
+
+        let handler;
+        if (isArray(underlying)) {
+            handler = this.createArrayHandler(path, proxiedChildren);
+        } else if (isMap(underlying)) {
+            handler = this.createMapHandler(path, proxiedChildren);
+        } else if (isSet(underlying)) {
+            handler = this.createSetHandler(path, proxiedChildren);
+        } else {
+            handler = this.createObjectHandler(path, proxiedChildren);
+        }
+
+        const proxy: BaseObject = new Proxy(underlying, handler);
 
         this.proxies.set(underlying, {
             path,
