f2df98b44b0ecbd6558c084753ac0659c41814e0
FTWinston <ftwinston@gmail.com>
2021-07-08 17:24:19 +0100

    Now exporting finishRecording from index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Patch, PatchType } from './Patch';
 export { applyChanges } from './applyChanges';
+export { finishRecording } from './finishRecording';
 export { recordChanges } from './recordChanges';
