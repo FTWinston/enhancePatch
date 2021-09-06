8e10bf004c4fb555a634e35511c14c0c717c2c70
FTWinston <ftwinston@gmail.com>
2021-09-06 22:09:24 +0100

    Added filterPatch and specific patch types to export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { Patch } from './Patch';
+export { Patch, ObjectPatch, ArrayPatch, MapPatch, SetPatch } from './Patch';
 export { applyChanges } from './applyChanges';
+export { filterPatch } from './filterPatch';
 export { finishRecording, finishRecordingRaw } from './finishRecording';
 export { recordChanges } from './recordChanges';
