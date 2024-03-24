0be1e0136078ade0de918eeb0c97711bd394edf8
FTWinston <ftwinston@gmail.com>
2024-03-24 16:20:44 +0000

    remove filterPatch function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { Patch, ObjectPatch, ArrayPatch, MapPatch, SetPatch } from './Patch';
 export { applyPatch } from './applyPatch';
-export { filterPatch } from './filterPatch';
 export { recordPatch } from './recordPatch';
