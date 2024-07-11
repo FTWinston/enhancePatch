ba630e619759dae648a7c40dd13226d4293af47a
FTWinston <ftwinston@gmail.com>
2024-07-12 00:16:10 +0100

    appendPatch now accounts for child patches

--- src/appendPatch.ts
+++ src/appendPatch.ts
@@ -1,5 +1,24 @@
-import { Patch } from './Patch';
+import { applyPatch } from './applyPatch';
+import { MapKey, Patch } from './Patch';
 import { isSet } from './typeChecks';
+
+function deleteMultiple<TKey>(deleteFrom: Map<TKey, unknown> | Set<TKey>, toDelete: Set<TKey> | IterableIterator<TKey>) {
+    for (const key of toDelete) {
+        deleteFrom.delete(key);
+    }
+}
+
+function addMultipleToSet<TKey>(addTo: Set<TKey>, toAdd: Set<TKey> | IterableIterator<TKey>) {
+    for (const key of toAdd) {
+        addTo.add(key);
+    }
+}
+
+function addMultipleToMap<TKey, TValue>(addTo: Map<TKey, TValue>, toAdd: Map<TKey, TValue>) {
+    for (const [key, value] of toAdd) {
+        addTo.set(key, value);
+    }
+}
 
 /**
  * Merge a newer patch into an older one, updating it so that it contains all changes from both patches.
@@ -9,22 +28,23 @@ import { isSet } from './typeChecks';
 export function appendPatch(target: Patch, addition: Patch) {
     if ('d' in addition && addition.d !== undefined) {
         if (isSet(addition.d)) {
-            // Add to target.d, and remove corresponding keys from target.s
+            // Add to target.d, and remove corresponding entries from target.s and target.c
             if ('d' in target && isSet(target.d)) {
-                for (const key of addition.d) {
-                    target.d.add(key as string); // key could be MapKey or string, but needs the type assertion here because appendPatch is agnostic as to which type of patch is being appended, as long as both patches are the same type.
-                }
+                addMultipleToSet(target.d, addition.d);
+            }
+            else {
+                (target as any).d = addition.d;
             }
 
             if ('s' in target && target.s !== undefined) {
-                for (const key of addition.d) {
-                    target.s.delete(key as string); // key could be MapKey or string, but needs the type assertion here because appendPatch is agnostic as to which type of patch is being appended, as long as both patches are the same type.
-                }
+                deleteMultiple(target.s, addition.d);
             }
             else if ('a' in target && target.a !== undefined) {
-                for (const key of addition.d) {
-                    target.a.delete(key);
-                }
+                deleteMultiple(target.a, addition.d);
+            }
+
+            if ('c' in target && target.c !== undefined) {
+                deleteMultiple(target.c, addition.d);
             }
         }
         else {
@@ -41,38 +61,65 @@ export function appendPatch(target: Patch, addition: Patch) {
     }
 
     if ('s' in addition && addition.s !== undefined) {
-        // Add to target.s, and remove corresponding keys from target.d
+        // Add to target.s, and remove corresponding entries from target.d and target.c
         if ('s' in target && target.s !== undefined) {
-            for (const [key, value] of addition.s) {
-                target.s.set(key as string, value); // key could be MapKey or string, but needs the type assertion here because appendPatch is agnostic as to which type of patch is being appended, as long as both patches are the same type.
-            }
+            addMultipleToMap(target.s, addition.s);
+        }
+        else {
+            (target as any).s = addition.s;
         }
 
         if ('d' in target && isSet(target.d)) {
-            for (const [key] of addition.s) {
-                target.d.delete(key as string); // key could be MapKey or string, but needs the type assertion here because appendPatch is agnostic as to which type of patch is being appended, as long as both patches are the same type.
-            }       
+            deleteMultiple(target.d, addition.s.keys());
+        }
+
+        if ('c' in target && target.c !== undefined) {
+            deleteMultiple(target.c, addition.s.keys());
         }
     }
     else if ('a' in addition && addition.a !== undefined) {
-        // Add to target.a, and remove corresponding keys from target.d
+        // Add to target.a, and remove corresponding entries from target.d
         if ('a' in target && target.a !== undefined) {
-            for (const key of addition.a) {
-                target.a.add(key);
-            }
+            addMultipleToSet(target.a, addition.a);
         }
 
         if ('d' in target && isSet(target.d)) {
-            for (const key of addition.a) {
-                target.d.delete(key as string); // key could be MapKey or string, but needs the type assertion here because appendPatch is agnostic as to which type of patch is being appended, as long as both patches are the same type.
-            }       
+            deleteMultiple(target.d, addition.a);
         }
     }
 
-    // TODO: c ... and also how that interacts with s & d
+    if ('c' in addition && addition.c !== undefined) {
+        // Add to target.c, unless we should instead update an existing entry in target.c or target.s
+        const targetC = 'c' in target && target.c !== undefined
+            ? target.c
+            : new Map<MapKey, Patch>();
+
+        for (const [key, childPatch] of addition.c) {
+            const targetPatchValue = targetC.get(key);
+
+            if (targetPatchValue !== undefined) {
+                // Target already has a patch for this child, so combine the two patches.
+                appendPatch(targetPatchValue, childPatch);
+                continue;
+            }
+            else if ('s' in target && target.s !== undefined) {
+                const targetSetValue = target.s.get(key as string);
+
+                if (targetSetValue !== undefined) {
+                    // Target sets this value, so just apply this child patch to that.
+                    applyPatch(targetSetValue, childPatch);
+                    continue;
+                }
+            }
+            
+            // Add to target.c
+            targetC.set(key, childPatch);
+        }
+
+        if (targetC.size > 0) {
+            (target as any).c = targetC;
+        }
+    }
 
     // TODO: arrayPatch o and c stuff ... this feels like reinventing the wheel.
-
-    // TODO: if addition modifies a child, and that child is actually set on target,
-    // shouldn't modify the child any more, right? Should just update it on target instead?
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Patch, ObjectPatch, ArrayPatch, MapPatch, SetPatch } from './Patch';
+export { appendPatch } from './appendPatch';
 export { applyPatch } from './applyPatch';
 export { recordPatch } from './recordPatch';
