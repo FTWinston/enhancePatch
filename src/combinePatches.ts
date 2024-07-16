bce41d87a702bce4474a8f529aad89a7545a8d74
FTWinston <ftwinston@gmail.com>
2024-07-17 00:09:18 +0100

    combinePatches no longer modifies provided patches

--- src/combinePatches.ts
+++ src/combinePatches.ts
@@ -4,67 +4,86 @@ import { ArrayPatch, MapKey, Patch } from './Patch';
 import { isSet } from './typeChecks';
 import { updateArrayPatchChildIndexes } from './updateArrayPatchChildIndexes';
 
-function deleteMultiple<TKey>(deleteFrom: Map<TKey, unknown> | Set<TKey>, toDelete: Set<TKey> | IterableIterator<TKey>) {
-    for (const key of toDelete) {
-        deleteFrom.delete(key);
+/** Add all items from one Set into another. */
+function combineSets<TKey>(
+    target: Set<TKey>,
+    additions: ReadonlySet<TKey> | IterableIterator<TKey>
+) {
+    for (const item of additions) {
+        target.add(item);
     }
 }
 
-function addMultipleToSet<TKey>(addTo: Set<TKey>, toAdd: Set<TKey> | IterableIterator<TKey>) {
-    for (const key of toAdd) {
-        addTo.add(key);
+/** Add all entries from one Map into another, overwriting existing values. */
+function combineMaps<TKey, TValue>(
+    target: Map<TKey, TValue>,
+    additions: ReadonlyMap<TKey, TValue> | Iterable<readonly [TKey, TValue]>
+) {
+    for (const [key, value] of additions) {
+        target.set(key, value);
     }
 }
 
-function addMultipleToMap<TKey, TValue>(addTo: Map<TKey, TValue>, toAdd: Map<TKey, TValue>) {
-    for (const [key, value] of toAdd) {
-        addTo.set(key, value);
+/** Remove all entries from Map whose key is present in a separate Set. */
+function removeKeys<TKey>(
+    target: Map<TKey, unknown> | Set<TKey>,
+    keysToExclude: ReadonlySet<TKey> | IterableIterator<TKey>
+) {
+    for (const key of keysToExclude) {
+        target.delete(key);
     }
 }
 
 /**
- * Merge newer patch(es) into an older one, updating it so that it contains all changes from all patches.
- * @param target The older patch, which will be updated with the addition.
- * @param additions The newer patch(es), which will be merged into the target, in order.
+ * Merge multiple patches into one, returning a new patch that contains all changes from all patches.
+ * @param patches All patches to be merged, in order from oldest to newest.
  */
-export function combinePatches(target: Patch, ...additions: Patch[]) {
-    for (const addition of additions) {
-        appendPatch(target, addition);
+export function combinePatches(...patches: Patch[]): Patch {
+    const result: Patch = { };
+
+    for (let i = 0; i < patches.length; i++) {
+        appendPatch(result, patches[i]);
     }
+
+    return result;
 }
 
+/**
+ * Merge a patch into a target patch, so that the target contains all changes from both patches.
+ * Modifies the target patch, but won't modify (or directly re-use members of) the addition.
+ */
 function appendPatch(target: Patch, addition: Patch) {
     // Add deletions, and potentially remove existing settings or child patches.
     if ('d' in addition && addition.d !== undefined) {
         if (isSet(addition.d)) {
             // Add to target.d, and remove corresponding entries from target.s and target.c
             if ('d' in target && isSet(target.d)) {
-                addMultipleToSet(target.d, addition.d);
+                combineSets(target.d, addition.d);
             }
             else {
-                (target as any).d = addition.d;
+                (target as any).d = new Set(addition.d);
             }
 
             if ('s' in target && target.s !== undefined) {
-                deleteMultiple(target.s, addition.d);
+                removeKeys(target.s, addition.d);
             }
             else if ('a' in target && target.a !== undefined) {
-                deleteMultiple(target.a, addition.d);
+                removeKeys(target.a, addition.d);
             }
 
             if ('c' in target && target.c !== undefined) {
-                deleteMultiple(target.c, addition.d);
+                removeKeys(target.c, addition.d);
             }
         }
         else {
-            // As addition.d is true, make target.d true, and clear target.s.
+            // As addition.d is true, make target.d true, and discard target.s and target.a.
             (target as any).d = true;
 
             if ('s' in target && target.s !== undefined) {
-                target.s.clear();
+                delete target.s;
             }
             else if ('a' in target && target.a !== undefined) {
-                target.a.clear();
+                delete target.a;
             }
         }
     }
@@ -73,28 +92,28 @@ function appendPatch(target: Patch, addition: Patch) {
     if ('s' in addition && addition.s !== undefined) {
         // Add to target.s, and remove corresponding entries from target.d and target.c
         if ('s' in target && target.s !== undefined) {
-            addMultipleToMap(target.s, addition.s);
+            combineMaps(target.s, addition.s);
         }
         else {
-            (target as any).s = addition.s;
+            (target as any).s = new Map(addition.s);
         }
 
         if ('d' in target && isSet(target.d)) {
-            deleteMultiple(target.d, addition.s.keys());
+            removeKeys(target.d, addition.s.keys());
         }
 
         if ('c' in target && target.c !== undefined) {
-            deleteMultiple(target.c, addition.s.keys());
+            removeKeys(target.c, addition.s.keys());
         }
     }
     else if ('a' in addition && addition.a !== undefined) {
         // Add to target.a, and remove corresponding entries from target.d
         if ('a' in target && target.a !== undefined) {
-            addMultipleToSet(target.a, addition.a);
+            combineSets(target.a, addition.a);
         }
 
         if ('d' in target && isSet(target.d)) {
-            deleteMultiple(target.d, addition.a);
+            removeKeys(target.d, addition.a);
         }
     }
 
@@ -104,7 +123,7 @@ function appendPatch(target: Patch, addition: Patch) {
             target.o = [...target.o, ...addition.o];
         }
         else {
-            (target as ArrayPatch).o = addition.o;
+            (target as ArrayPatch).o = [...addition.o];
         }
 
         if ('c' in target && target.c !== undefined) {
@@ -121,9 +140,9 @@ function appendPatch(target: Patch, addition: Patch) {
 
     // Add child patches, potentially updating corresponding existing child patches.
     if ('c' in addition && addition.c !== undefined) {
-        // Add to target.c, unless we should instead update an existing entry in target.c or target.s
+        // Add to result.c, unless we should instead update an existing entry in target.c or target.s
         const targetC = 'c' in target && target.c !== undefined
-            ? target.c
+            ? new Map<MapKey, Patch>(target.c)
             : new Map<MapKey, Patch>();
 
         for (const [key, childPatch] of addition.c) {
@@ -138,7 +157,7 @@ function appendPatch(target: Patch, addition: Patch) {
                 const targetSetValue = target.s.get(key as string);
 
                 if (targetSetValue !== undefined) {
-                    // Target sets this value, so just apply this child patch to that.
+                    // Result sets this value, so just apply this child patch to the value it sets.
                     applyPatch(targetSetValue, childPatch);
                     continue;
                 }
